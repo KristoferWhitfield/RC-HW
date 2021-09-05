2e1fd0b1870960f3c587ec97632ae64ee7fd72ca
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-09-05 17:49:54 -0400

    template

--- Week 4/template/js/main.js
+++ Week 4/template/js/main.js
@@ -11,18 +11,48 @@
 //   document.querySelector("#result").innerText = dog + cat
 // }
 
+// for(let i = 1; i < 101; i++){
+//   if(i % 15 === 0){
+//     console.log("Fizzbuzz")
+//   }else if (i % 3 === 0){
+//     console.log("Fizz")
+//   }else if (i % 5 === 0) {
+//     console.log("Buzz")
+//   }else {
+//       console.log(i)
+//   }
+// }
 
 
-
-
-for(let i = 1; i < 101; i++){
-  if(i % 15 === 0){
-    console.log("Fizzbuzz")
-  }else if (i % 3 === 0){
-    console.log("Fizz")
-  }else if (i % 5 === 0) {
-    console.log("Buzz")
-  }else {
-      console.log(i)
-  }
-}
+// document.getElementById("enter").addEventListener("click", test)
+//
+// function test () {
+//     let month= document.getElementById("month").value.toLowerCase()
+//     let day= document.getElementById("day").value
+//     let result= document.getElementById("results")
+//         if (month === 'march' && day >= 21 || month === 'april' && day <= 19){
+//             result.innerText = "aries"
+// }else if (month === 'april' && day >= 20 || month === 'may' && day <= 20){
+//             result.innerText ="taurus"
+// }else if (month === 'may' && day >= 21 || month === 'june' && day <= 21){
+//             result.innerText ="gemini"
+// }else if (month === 'june' && day >= 22 || month === 'july' && day <= 22){
+//             result.innerText ="cancer"
+// }else if (month === 'july' && day >= 23 || month === 'august' && day <= 22){
+//             result.innerText ="leo"
+// }else if (month === 'august' && day >= 23 || month === 'september' && day <= 22){
+//             result.innerText ="virgo"
+// }else if (month === 'september' && day >= 23 || month === 'october' && day <= 23){
+//             result.innerText ="libra"
+// }else if (month === 'october' && day >= 24 || month === 'november' && day <= 21){
+//             result.innerText ="scorpius"
+// }else if (month === 'november' && day >= 22 || month === 'december' && day <= 21){
+//             result.innerText ="sagittarius"
+// }else if (month === 'december' && day >= 22 || month === 'january' && day <= 19){
+//             result.innerText ="capricornus"
+// }else if (month === 'january' && day >= 20 || month === 'february' && day <= 18){
+//             result.innerText ="aquarius"
+// }else if (month === 'february' && day >= 19 || month === 'march' && day <= 20){
+//             result.innerText ="pisces"
+// }
+// }
