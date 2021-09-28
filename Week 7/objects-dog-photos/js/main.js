eb964f5b09a7188bf3f31a078fff25179842f05a
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-09-28 18:20:50 -0400

    weather api

--- Week 7/objects-dog-photos/js/main.js
+++ Week 7/objects-dog-photos/js/main.js
@@ -1,1 +1,20 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+function dogRandom(){
+
+const url = 'https://dog.ceo/api/breeds/image/random'
+
+fetch(url)
+    .then(res => res.json())
+
+    .then(data => {
+      console.log(data.message)
+      document.querySelector('img').src = data.message
+    }
+    })
+    .catch(err => {
+      console.log(`error =${err}`)
+    })
+
+
+}
